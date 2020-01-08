d464730f1331dc17234e37a41c74f8e1f2972c40
vibecode <rumashkin@gmail.com>
2020-01-08 21:59:48 -0100

    handle inputs state

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -8,12 +8,15 @@ import React, {
 import { Route, Switch } from 'react-router-dom'
 import GlobalFeed from 'pages/GlobalFeed'
 import Article from 'pages/Article'
+import Authentication from 'pages/authentication'
 
 export default () => {
   return (
     <Switch>
       <Route path="/" component={GlobalFeed} exact></Route>
       <Route path="/article/:slug" component={Article}></Route>
+      <Route path="/register" component={Authentication}></Route>
+      <Route path="/login" component={Authentication}></Route>
     </Switch>
   )
 }
